453a02f5412d5e287a68e80e214060ebfdb98c68
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-04-10 09:34:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+noseY = "";
+noseX = "";
 function setup() {
     canvas = createCanvas(640, 480);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    pose_net = ml5.poseNet(video, modelloaded)
+    pose_net.on("pose", gotposes)
 }
 
 function draw() {
@@ -11,4 +15,17 @@ function draw() {
 
 function takesnap() {
     save("mustacheuser.png");
+}
+
+function modelloaded() {
+    console.log("posenet is initialized");
+}
+
+function gotposes(results) {
+    if(results.length > 0) {
+        console.log(results);
+        noseY = results[0].pose.nose.y;
+        noseX = results[0].pose.nose.x;
+        console.log("noseY = " + noseY + " noseX = " + noseX);
+    }
 }
